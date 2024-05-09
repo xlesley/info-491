c706af628b8d9e4114375e36b1e97c6832ddadf3
xlesley <114975168+xlesley@users.noreply.github.com>
2024-05-08 21:17:51 -0700

    Update the database

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,19 @@ export function Footer(props) {
   return (
       <footer className="footer-content  animate__animated">
           <div className="container">
-              <div className="column">
+              {/* <div className="column">
                   <h2>Menu</h2>
                   <a href="#services">Services</a>
                   <a href="#work">Work</a>
                   <a href="#about">About</a>
                   <a href="#contact">Contact</a>
-              </div>
+              </div> */}
               <div className="column">
-                  <h2>Social</h2>
-                  /* TODO: Change to actual account when we got one */
-                  <a href="https://instagram.com">Instagram</a>
-                  <a href="https://facebook.com">Facebook</a>
+                  <h3>Disclaimer</h3>
+                  <p>PetLife is a community platform for sharing pet-related content. We do not verify the accuracy or reliability of any information posted. The content provided does not constitute veterinary advice. Use of our site is at your own risk, and we are not liable for any misinformation or damages resulting from the use of information provided by users.</p>
+                  <h3>Connect with us!</h3>
+                  <a href= "mailto: jingrui@uw.edu">jingrui@uw.edu</a>
+                  <a href= "mailto: jingrui@uw.edu">dexinsun@uw.edu</a>
               </div>
 
           </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import './index.css';
+
+
 import { App } from './components/App';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import firebase from 'firebase/compat/app';
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/*
+const firebaseConfig = {
+    apiKey: "AIzaSyBLNyRbt6XDyPB4t_XQb6w_m7Za1dZjAcM",
+    authDomain: "info340-78916.firebaseapp.com",
+    projectId: "info340-78916",
+    storageBucket: "info340-78916.appspot.com",
+    messagingSenderId: "41115156677",
+    appId: "1:41115156677:web:f4aaf5e80e4a6fe79a70ac",
+    measurementId: "G-JCMEWX3LY3"
+};
+
+ */
+/*
 const firebaseConfig = {
     apiKey: "AIzaSyDvT6pBRKlWyaDxIjWYFnsgRQwJlGZlI6U",
     authDomain: "info340-campus-connect.firebaseapp.com",
@@ -15,6 +35,18 @@ const firebaseConfig = {
     storageBucket: "info340-campus-connect.appspot.com",
     messagingSenderId: "50803215044",
     appId: "1:50803215044:web:ce067cf2970cdc1780d791"
+};
+
+ */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOeA0rvsa2-iKKOw1wqfd5XUCbGZTpD1s",
+    authDomain: "info490-faa56.firebaseapp.com",
+    projectId: "info490-faa56",
+    storageBucket: "info490-faa56.appspot.com",
+    messagingSenderId: "407184466844",
+    appId: "1:407184466844:web:6ce28d78693b7ae67d1e59",
+    measurementId: "G-F0DYM840X0"
 };
 firebase.initializeApp(firebaseConfig);
 // Initialize Firebase
